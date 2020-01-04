fc31f1d39d222fbc788efcbc671c3bcb22b73dbd
Don Boyd <donboyd5@gmail.com>
2020-01-04 08:41:13 -0500

    Good run

--- temp.r
+++ temp.r
@@ -1,3 +1,126 @@
+runtso <- function(url) {
+  out <- tryCatch(
+    {
+      
+      message("This is the 'try' part")
+      
+      tso
+    },
+    error=function(cond) {
+      return(make_empty_row(vec))
+    },
+    warning=function(cond) {
+      return(NULL)
+    },
+    finally={
+    }
+  )    
+  return(out)
+}
+
+
+log_calculator <- function(x){
+  tryCatch(
+    expr = {
+      message(log(x))
+      message("Successfully executed the log(x) call.")
+    },
+    error = function(e){
+      message('Caught an error!')
+      print(e)
+    },
+    warning = function(w){
+      message('Caught an warning!')
+      print(w)
+    },
+    finally = {
+      message('All done, quitting.')
+    }
+  )    
+}
+
+
+log_calculator(10)
+log_calculator(-10)
+log_calculator("a")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+readUrl <- function(url) {
+  out <- tryCatch(
+    {
+      # Just to highlight: if you want to use more than one 
+      # R expression in the "try" part then you'll have to 
+      # use curly brackets.
+      # 'tryCatch()' will return the last evaluated expression 
+      # in case the "try" part was completed successfully
+      
+      message("This is the 'try' part")
+      
+      readLines(con=url, warn=FALSE) 
+      # The return value of `readLines()` is the actual value 
+      # that will be returned in case there is no condition 
+      # (e.g. warning or error). 
+      # You don't need to state the return value via `return()` as code 
+      # in the "try" part is not wrapped insided a function (unlike that
+      # for the condition handlers for warnings and error below)
+    },
+    error=function(cond) {
+      message(paste("URL does not seem to exist:", url))
+      message("Here's the original error message:")
+      message(cond)
+      # Choose a return value in case of error
+      return(NA)
+    },
+    warning=function(cond) {
+      message(paste("URL caused a warning:", url))
+      message("Here's the original warning message:")
+      message(cond)
+      # Choose a return value in case of warning
+      return(NULL)
+    },
+    finally={
+      # NOTE:
+      # Here goes everything that should be executed at the end,
+      # regardless of success or error.
+      # If you want more than one expression to be executed, then you 
+      # need to wrap them in curly brackets ({...}); otherwise you could
+      # just have written 'finally=<expression>' 
+      message(paste("Processed URL:", url))
+      message("Some other message at the end")
+    }
+  )    
+  return(out)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # https://stackoverflow.com/questions/7233288/non-stationary-seasonal-ar-part-from-css-error-in-r
 
 glimpse(results)
